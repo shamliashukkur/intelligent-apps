278d0bf9ff78b02ffc5747a12246827c1746a994
Shamlia Shukkur <shamlia@live.com>
2018-12-30 18:08:12 +0400

    Completed Task 3 - state - PBI1

--- ContosoHelpdeskChatBot/ContosoHelpdeskChatBot/BotAccessors.cs
+++ ContosoHelpdeskChatBot/ContosoHelpdeskChatBot/BotAccessors.cs
@@ -15,8 +15,8 @@ namespace ContosoHelpdeskChatBot
             ConversationState = conversationState ?? throw new ArgumentNullException(nameof(conversationState));
         }
 
-        //public static string ContosoBotStateAccessorName { get; } = $"{nameof(BotAccessors)}.ContosoBotState";
-        //public IStatePropertyAccessor<ContosoBotState> ContosoBotStateStateAccessor { get; internal set; }
+        public static string ContosoBotStateAccessorName { get; } = $"{nameof(BotAccessors)}.ContosoBotState";
+        public IStatePropertyAccessor<ContosoBotState> ContosoBotStateStateAccessor { get; internal set; }
 
         public static string DialogStateAccessorName { get; } = $"{nameof(BotAccessors)}.DialogState";
         public IStatePropertyAccessor<DialogState> DialogStateAccessor { get; internal set; }
